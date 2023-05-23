95a10f75cad86a37f791dc3f491cf64f561754d7
Vikas preet Tiwana <vikastiwana8@gmail.com>
2023-05-22 18:09:05 -0700

    first deliverable done-get all ramen objects and display them

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 // write your code here
+function getAllRamenObjects() {
+  fetch("http://localhost:3000/ramens")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      displayRamenImages(data);
+      displayInfo(data);
+    });
+}
+getAllRamenObjects();
+
+// task-1: display each ramen image
+function displayRamenImages(ramenArray) {
+  const ramenMenu = document.getElementById("ramen-menu");
+  ramenArray.forEach((ramen) => {
+    const image = document.createElement("img");
+    image.src = ramen.image;
+    ramenMenu.append(image);
+  });
+}
+
+//Task-2: see details and inser comment and rating
+document.getElementById("ramen-menu").addEventListener("click", displayInfo);
+
+function displayInfo(event) {
+  const ramenImageDetail = document.getElementsByClassName("detail-image");
+}
